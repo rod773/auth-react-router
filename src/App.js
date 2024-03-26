0759b1a2d7a8f38d85df4323728559abc284a07f
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-26 16:56:50 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/Home";
 import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+import { Secret } from "./pages/Secret";
+import "./App.css";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/secret"
+          element={
+            <ProtectedRoute>
+              <Secret />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
-
 export default App;
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
 
@@ -6,27 +6,38 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage("user", null);
+  const [is2FAVerified, setIs2FAVerified] = useState(false);
   const navigate = useNavigate();
-  // call this function when you want to authenticate the user
   const login = async (data) => {
     setUser(data);
-    navigate("/profile");
+    // Navigate to 2FA verification page
+    navigate("/verify-2fa");
   };
-  // call this function to sign out logged in user
   const logout = () => {
     setUser(null);
+    setIs2FAVerified(false);
     navigate("/", { replace: true });
   };
-  const value = useMemo(
-    () => ({
-      user,
-      login,
-      logout,
-    }),
-    [user]
-  );
+
+  const verify2FACode = async (code) => {
+    // Mock verification logic
+    if (code === "0000") {
+      setIs2FAVerified(true);
+      navigate("/secret"); // Navigate to a protected route after successful 2FA
+      return true;
+    }
+    return false;
+  };
+  const value = {
+    user,
+    is2FAVerified,
+    login,
+    logout,
+    verify2FACode,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
+
 export const useAuth = () => {
   return useContext(AuthContext);
 };
