d919a779cf243d9d07db18a58394b4058489c647
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-26 17:16:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { HomePage } from "./pages/Home";
-import { Secret } from "./pages/Secret";
-import { Verify2FA } from "./pages/Verify2FA";
+import { LoginPage } from "pages/Login";
+import { HomePage } from "pages/Home";
+import { Secret } from "pages/Secret";
+import { Verify2FA } from "pages/Verify2FA";
 import "./App.css";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { AuthProvider } from "./hooks/useAuth";
+import { ProtectedRoute } from "components/ProtectedRoute";
+import { AuthProvider } from "hooks/useAuth";
 
 function App() {
   return (
