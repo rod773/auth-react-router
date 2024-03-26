24afb398f3368c84fe5ede9dab884d5198f924ec
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-26 16:38:00 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,9 @@
+import { useAuth } from "hooks/useAuth";
+import { useState } from "react";
+
 export const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return <h1>Login Page</h1>;
 };
