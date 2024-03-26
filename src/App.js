d4052a6b2ca0a1578e4faf51027598b561356197
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-26 17:00:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { HomePage } from "./pages/Home";
-import { Secret } from "./pages/Secret";
+import { LoginPage } from "pages/Login";
+import { HomePage } from "pages/Home";
+import { Secret } from "pages/Secret";
+import { Verify2FA } from "./pages/Verify2FA";
 import "./App.css";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { AuthProvider } from "./hooks/useAuth";
+import { ProtectedRoute } from "components/ProtectedRoute";
+import { AuthProvider } from "hooks/useAuth";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/verify-2fa" element={<Verify2FA />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/secret"
@@ -24,4 +26,3 @@ function App() {
     </AuthProvider>
   );
 }
-export default App;
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,27 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { HomePage } from "./pages/Home";
-import { Secret } from "./pages/Secret";
-import { Verify2FA } from "./pages/Verify2FA";
-import "./App.css";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { AuthProvider } from "./hooks/useAuth";
-function App() {
-  return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/verify-2fa" element={<Verify2FA />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/secret"
-          element={
-            <ProtectedRoute>
-              <Secret />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </AuthProvider>
-  );
-}
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  if (!user) {
+    // user is not authenticated
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
